387bbfa1e98b7ae644defe4914433f80424fc5ac
Brice Duke <bricewduke@gmail.com>
2023-03-29 14:21:40 -0500

    chore(global): add sentry
    Added sentry to watch the server and client side, includng a custom _error page to catch unhandled
    errors (they'll be sent to sentry now)

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,21 @@
+import { env } from "@/env.mjs";
+import { appRouter } from "@/server/api/root";
+import { createTRPCContext } from "@/server/api/trpc";
+import { wrapApiHandlerWithSentry } from "@sentry/nextjs";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "@/env.mjs";
-import { createTRPCContext } from "@/server/api/trpc";
-import { appRouter } from "@/server/api/root";
-
 // export API handler
-export default createNextApiHandler({
+const handler = createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
       : undefined,
 });
+
+export default wrapApiHandlerWithSentry(handler, "/api/[trpc]");
