445fd4f62a75f1a3ed7819f210d8447e3e9989a6
Brice Duke <bricewduke@gmail.com>
2023-04-01 13:48:19 -0500

    fix(sentry): switch from env file to process env
    fix #11

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,15 +1,13 @@
 // This file configures the initialization of Sentry on the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import { env } from "@/env.mjs";
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = env.SENTRY_DSN || env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  enabled: env.NODE_ENV === "production",
+  enabled: process.env.NODE_ENV === "production",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,14 +2,13 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { env } from "@/env.mjs";
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = env.SENTRY_DSN || env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  enabled: env.NODE_ENV === "production",
+  enabled: process.env.NODE_ENV === "production",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
