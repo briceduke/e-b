a5dab9f2ff9617000945a6e13e7a265b79be5088
Brice Duke <bricewduke@gmail.com>
2023-04-01 13:31:02 -0500

    feat(modals): add consistent styles to modals
    Added titles and formatted each modal.

--- src/components/modals/auth.modal.tsx
+++ src/components/modals/auth.modal.tsx
@@ -1,9 +1,19 @@
 import { useAtom } from 'jotai';
 import { signIn } from 'next-auth/react';
+import { BiLogIn } from 'react-icons/bi';
 import { FcGoogle } from 'react-icons/fc';
 
 import { authModalOpenAtom } from '@/store/modals.atom';
-import { Button, Modal, Title } from '@mantine/core';
+import { Box, Button, Modal, Text, ThemeIcon, Title } from '@mantine/core';
+
+const ModalTitle = () => {
+    return <Box display={'flex'} className='items-center space-x-4'>
+        <ThemeIcon variant='light'>
+            <BiLogIn />
+        </ThemeIcon>
+        <Title order={3}>Sign In</Title>
+    </Box>
+}
 
 export const AuthModal = () => {
     const [authModalOpen, setAuthModalOpen] = useAtom(authModalOpenAtom);
@@ -12,11 +22,17 @@ export const AuthModal = () => {
         void signIn('google');
     }
 
-    return <Modal opened={authModalOpen} onClose={() => setAuthModalOpen(false)} centered radius={'md'}>
-        <div className='flex flex-col items-center space-y-4 p-4'>
-            <Title>Welcome Back!</Title>
+    return <Modal
+        opened={authModalOpen}
+        onClose={() => setAuthModalOpen(false)}
+        centered
+        radius={'md'}
+        title={<ModalTitle />}
+    >
+        <div className='space-y-4'>
+            <Text>Sign in with:</Text>
 
-            <Button leftIcon={<FcGoogle />} onClick={handleSignIn} variant="default" color="gray">Continue with Google</Button>
+            <Button leftIcon={<FcGoogle />} onClick={handleSignIn} variant='light'>Google</Button>
         </div>
     </Modal>
 }
